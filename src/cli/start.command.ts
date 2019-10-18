2d4a4b94356c33687d092e16b04c1abc1741ab27
1053910251 <1053910251@qq.com>
2019-10-18 14:05:56 +0800

    add start command

--- src/cli/start.command.ts
+++ src/cli/start.command.ts
@@ -2,6 +2,7 @@ import { AbstractCommand } from './abstract.command';
 import { CommanderStatic } from 'commander';
 import { resolve } from 'path';
 import * as shelljs from 'shelljs';
+import { existsSync } from 'fs';
 
 export class StartCommand extends AbstractCommand {
   private frameworkRoot: string;
@@ -16,10 +17,17 @@ export class StartCommand extends AbstractCommand {
       .command('start')
       .action(() => {
         const projectDir = process.cwd();
+        const modulesDir = resolve(this.frameworkRoot, 'modules');
         const pkgJson = require(resolve(projectDir, 'package.json'));
+        const appName = pkgJson.name.replace(/nest-app-/g, '');
+        const appDir = resolve(modulesDir, appName);
         // 构建
         shelljs.exec('npm run build');
-        shelljs.cp('-R', resolve(projectDir, 'dist'), resolve(this.frameworkRoot, 'modules', pkgJson.name));
+        // 先删除再cp
+        if (existsSync(appDir)) {
+          shelljs.rm('-rf', appDir);
+        }
+        shelljs.cp('-R', resolve(projectDir, 'dist'), appDir);
         shelljs.cd(this.frameworkRoot);
         shelljs.exec('npm run start:prod');
       });
