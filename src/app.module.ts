be6e1f0cd0c7bae72da9d7ff6d51792e5f48292d
xp-fly <793832050@qq.com>
2019-10-24 23:04:34 +0800

    [feature] fix bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModuleLoader } from './module-loader';
 import { ConfigModule } from 'nestjs-configure';
-import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
     imports: [
         ConfigModule.load(),
-        DatabaseModule,
         ...ModuleLoader.load(),
     ],
     controllers: [AppController],
